4d94ce95fba11d965fa383e90ed6e535fef54966
Alexander <nicky-nico-1998@outlook.com>
2023-06-20 13:04:37 +0100

    new file create to store cards information. Two cards infomation added.

---
+++ src/assets/cardsData.js
@@ -0,0 +1,18 @@
+export const cardsData = [
+    {
+        id: 1,
+        title: "E-commerce Website",
+        image: require('../images/cardsData-1.jpg'),
+        description: "Make-Up Junkie is an e-commerce website of make-up products.",
+        github: 'https://github.com/OlgaFelczak/eComReactApp',
+        website: 'https://ecom-react-app-makeup-junkie.netlify.app/',
+    },
+    {
+        id: 2,
+        title: "Weather Forecast",
+        image: require('../images/cardsData-2.jpg'),
+        description: "Check the forecast of any city in real time.",
+        github: 'https://github.com/PurAlex/Weather-Forecast',
+        website: 'https://puralex.github.io/Weather-Forecast/',
+    },
+]
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,33 +1,32 @@
 import Card from 'react-bootstrap/Card';
 import '../styles/projects.css';
+import Button from 'react-bootstrap/Button';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import { Container } from 'react-bootstrap';
 
 function Projects() {
     return (
-        <div className='cards-group'>
-            <Card className="bg-dark text-white project-card">
-                <Card.Img src="holder.js/100px270" alt="Card image" />
-                <Card.ImgOverlay>
-                    <Card.Title>Card title</Card.Title>
-                    <Card.Text>
-                        This is a wider card with supporting text below as a natural lead-in
-                        to additional content. This content is a little bit longer.
-                    </Card.Text>
-                    <Card.Text>Last updated 3 mins ago</Card.Text>
-                </Card.ImgOverlay>
-            </Card>
+        <Container>
+            <Row>
+                {/* <div className='cards-group'> */}
+                <Col sm={4}>
+                    <Card className="bg-dark text-white project-card">
+                        <Card.Img src="holder.js/100px270" alt="Project image" />
+                        <Card.ImgOverlay>
+                            <Card.Title>E-commerce Website</Card.Title>
+                            <Card.Text>
+                                Make-Up Junkie is an e-commerce website of make-up products.
+                            </Card.Text>
+                            <Button variant="outline-primary">Primary</Button>{' '}
+                            <Button variant="outline-primary">Primary</Button>{' '}
+                        </Card.ImgOverlay>
+                    </Card>
+                </Col>
 
-            <Card className="bg-dark text-white project-card">
-                <Card.Img src="holder.js/100px270" alt="Card image" />
-                <Card.ImgOverlay>
-                    <Card.Title>Card title</Card.Title>
-                    <Card.Text>
-                        This is a wider card with supporting text below as a natural lead-in
-                        to additional content. This content is a little bit longer.
-                    </Card.Text>
-                    <Card.Text>Last updated 3 mins ago</Card.Text>
-                </Card.ImgOverlay>
-            </Card>
-        </div>
+                {/* </div> */}
+            </Row>
+        </Container>
     )
 }
 
