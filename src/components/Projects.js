967c38e00ce22edc8dced3095828c5f066a981b0
Alexander <nicky-nico-1998@outlook.com>
2023-06-20 13:15:30 +0100

    cardsData information export to Projects.

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -4,25 +4,28 @@ import Button from 'react-bootstrap/Button';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import { Container } from 'react-bootstrap';
+import { cardsData } from '../assets/cardsData';
 
 function Projects() {
     return (
-        <Container>
+        <Container fluid>
             <Row>
+                {cardsData.map((data) => (
+                    <Col sm={4}>
+                        <Card className="bg-dark text-white project-card">
+                            <Card.Img src={data.image} alt="Project image" />
+                            <Card.ImgOverlay>
+                                <Card.Title>{data.title}</Card.Title>
+                                <Card.Text>
+                                    {data.description}
+                                </Card.Text>
+                                <Button variant="outline-primary">Primary</Button>{' '}
+                                <Button variant="outline-primary">Primary</Button>{' '}
+                            </Card.ImgOverlay>
+                        </Card>
+                    </Col>
+                ))}
                 {/* <div className='cards-group'> */}
-                <Col sm={4}>
-                    <Card className="bg-dark text-white project-card">
-                        <Card.Img src="holder.js/100px270" alt="Project image" />
-                        <Card.ImgOverlay>
-                            <Card.Title>E-commerce Website</Card.Title>
-                            <Card.Text>
-                                Make-Up Junkie is an e-commerce website of make-up products.
-                            </Card.Text>
-                            <Button variant="outline-primary">Primary</Button>{' '}
-                            <Button variant="outline-primary">Primary</Button>{' '}
-                        </Card.ImgOverlay>
-                    </Card>
-                </Col>
 
                 {/* </div> */}
             </Row>
