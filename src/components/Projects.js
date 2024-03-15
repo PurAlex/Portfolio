a9312b348f621dca4ed6bf32db2a7983ce7f5005
Alexander <nicky-nico-1998@outlook.com>
2024-03-15 13:37:57 +0000

    Card information and images added to each card

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,6 +1,11 @@
 // import Card from 'react-bootstrap/Card';
 import '../styles/projects.css';
 // import Button from 'react-bootstrap/Button';
+import cardimage1 from '../images/cardsData-1.jpg';
+import cardimage2 from '../images/cardsData-2.jpg';
+import cardimage3 from '../images/cardsData-3.jpg';
+import cardimage4 from '../images/cardsData-4.jpg';
+
 
 function Projects() {
     return (
@@ -8,50 +13,46 @@ function Projects() {
 
             <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-3">
                 <div class="col">
-                    <div class="card text-bg-dark">
-                        <img src="https://mdbcdn.b-cdn.net/img/new/standard/city/041.webp" class="card-img-top" alt="Hollywood Sign on The Hill" />
+                    <div class="card text-bg-light">
+                        <img src={cardimage1} class="card-img-top" alt="Hollywood Sign on The Hill" />
                         <div class="card-img-overlay">
-                            <h5 class="card-title">Card title</h5>
+                            <h5 class="card-title">E-commerce Website</h5>
                             <p class="card-text">
-                                This is a longer card with supporting text below as a natural lead-in to
-                                additional content. This content is a little bit longer.
+                                Make-Up Junkie is an e-commerce website of make-up products.
                             </p>
                         </div>
                     </div>
                 </div>
                 <div class="col">
-                    <div class="card text-bg-dark">
-                        <img src="https://mdbcdn.b-cdn.net/img/new/standard/city/041.webp" class="card-img-top" alt="Hollywood Sign on The Hill" />
+                    <div class="card text-bg-light">
+                        <img src={cardimage4} class="card-img-top" alt="Hollywood Sign on The Hill" />
                         <div class="card-img-overlay">
-                            <h5 class="card-title">Card title</h5>
+                            <h5 class="card-title">Quiz Challenge</h5>
                             <p class="card-text">
-                                This is a longer card with supporting text below as a natural lead-in to
-                                additional content. This content is a little bit longer.
+                                Multiple choices and questions quiz.
                             </p>
                         </div>
                     </div>
                 </div>
                 <div class="col">
-                    <div class="card text-bg-dark">
-                        <img src="https://mdbcdn.b-cdn.net/img/new/standard/city/041.webp" class="card-img-top" alt="Hollywood Sign on The Hill" />
+                    <div class="card text-bg-light">
+                        <img src={cardimage2} class="card-img-top" alt="Hollywood Sign on The Hill" />
                         <div class="card-img-overlay">
-                            <h5 class="card-title">Card title</h5>
+                            <h5 class="card-title">Weather Forecast</h5>
                             <p class="card-text">
-                                This is a longer card with supporting text below as a natural lead-in to
-                                additional content. This content is a little bit longer.
+                                Check the forecast of any city in real time.
                             </p>
                         </div>
                     </div>
                 </div>
 
                 <div class="col">
-                    <div class="card text-bg-dark">
-                        <img src="https://mdbcdn.b-cdn.net/img/new/standard/city/041.webp" class="card-img-top" alt="Hollywood Sign on The Hill" />
+                    <div class="card text-bg-light">
+                        <img src={cardimage3} class="card-img-top" alt="Hollywood Sign on The Hill" />
                         <div class="card-img-overlay">
-                            <h5 class="card-title">Card title</h5>
+                            <h5 class="card-title">Team Profile</h5>
                             <p class="card-text">
-                                This is a longer card with supporting text below as a natural lead-in to
-                                additional content. This content is a little bit longer.
+                                Generate dynamically a team profile based on user input.
                             </p>
                         </div>
                     </div>
